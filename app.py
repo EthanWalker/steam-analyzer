255dd7799d35d934e5954efb80ffbef440b22d43
Ethan Walker <ewalker15@yahoo.com>
2017-02-04 14:15:22 -0600

    Added favicon, requirements, and changed steam request

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
-from steam import *
-from flask import Flask
-from flask import render_template
-
+from steam import (
+    get_avatar,
+    get_friends_list,
+    get_game_list,
+    get_persona_name,
+)
+from flask import Flask, render_template, send_from_directory
+import os
 
 app = Flask(__name__)
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),'favicon.ico')
+
 
 @app.route('/')
 @app.route('/<steam_id>')
@@ -12,7 +21,12 @@ def index(steam_id="76561197960435530"):
     game_count = get_game_list(steam_id)["count"]
     friends_count = len(get_friends_list(steam_id))
     avatar = get_avatar(steam_id)
-    context = {'username': username, "game_count": game_count, "friends_count": friends_count, "avatar": avatar}
+    context = {
+        "username": username,
+        "game_count": game_count,
+        "friends_count": friends_count,
+        "avatar": avatar,
+    }
     return render_template("index.html", **context)
 
 
--- steam.py
+++ steam.py
@@ -1,47 +1,81 @@
 import json
 import requests
 import requests_cache
-from decimal import *
-getcontext().prec = 2
 
 requests_cache.install_cache('api_cache')
 
 APIPATH = r'C:\api_keys.json'
 cred = json.load(open(APIPATH))
-
-STEAM_API_URL = "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002"
 STEAM_API_KEY = cred["STEAM_API_KEY"]
+STEAM_USER_URL = "http://api.steampowered.com/ISteamUser/{}/{}/"
+STEAM_PLAYER_URL = "http://api.steampowered.com/IPlayerService/{}/{}/"
+VERSION = 'v0002'
 
 
-def get_persona_name(username):
+
+def make_steam_request(steam_url, endpoint, version, payload):
+    """
+    Make a GET request to a steam endpoint with the provided version.
+
+    :param endpoint: string endpoint attached to STEAM_USER_URL
+    :param version: string 'v0001' or 'v0002'
+    :param payload: dictionary of GET params to send to endpoint
+    :return: json response from server
+    """
+
+    url = steam_url.format(endpoint, version)
+    response = requests.get(url, params=payload).json()
+    return response
+
+
+def get_persona_name(steam_id):
    # get custom player summary using key and provided user ID
-   player_summary =  requests.get("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={0}&steamids={1}".format( STEAM_API_KEY, username)).json()
+   payload = {
+       'key': STEAM_API_KEY,
+       'steamids': steam_id,
+   }
+   json_response = make_steam_request(
+       steam_url = STEAM_USER_URL,
+       endpoint = "GetPlayerSummaries",
+       version = VERSION,
+       payload = payload,
+   )
    # drill down into player summary JSON and get persona name
-   persona_name = player_summary["response"]["players"][0]["personaname"]
+   persona_name = json_response["response"]["players"][0]["personaname"]
    # return the persona name
    return persona_name
 
-def get_avatar(username):
+def get_avatar(steam_id):
    # get custom player summary using key and provided user ID
-   player_summary =  requests.get("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={0}&steamids={1}".format( STEAM_API_KEY, username)).json()
-   # drill down into player summary JSON and get persona name
-   avatar = player_summary["response"]["players"][0]["avatarfull"]
-   # return the persona name
+   payload = {
+       'key': STEAM_API_KEY,
+       'steamids': steam_id,
+   }
+   json_response = make_steam_request(
+       steam_url = STEAM_USER_URL,
+       endpoint = "GetPlayerSummaries",
+       version = VERSION,
+       payload = payload,
+   )
+   # drill down into player summary JSON and get avatar
+   avatar = json_response["response"]["players"][0]["avatarfull"]
+   # return the avatar
    return avatar
 
 
-def get_friends_list(username):
+def get_friends_list(steam_id):
    # get custom friends list using key and provided user ID
-   friends_json =  requests.get("http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key={0}&steamid={1}&relationship=friend".format( STEAM_API_KEY, username)).json()
+
+   friends_json =  requests.get("http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key={0}&steamid={1}&relationship=friend".format( STEAM_API_KEY, steam_id)).json()
    # drill down into JSON and get friends list
    friends_list = friends_json["friendslist"]["friends"]
    # return the friends list
    return friends_list
 
 
-def get_game_list(username):
+def get_game_list(steam_id):
    # get custom games list using key and provided user ID
-   games_json =  requests.get("http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={0}&steamid={1}&include_appinfo=1".format( STEAM_API_KEY, username)).json()
+   games_json =  requests.get("http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={0}&steamid={1}&include_appinfo=1".format( STEAM_API_KEY, steam_id)).json()
    # drill down into JSON and get games list and count
    game_list = games_json["response"]["games"]
    game_count = games_json["response"]["game_count"]
