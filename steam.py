2b9d166f81c8f3df57427cb1936adfb213053754
Ethan Walker <ewalker15@yahoo.com>
2017-02-06 17:10:07 -0600

    changed how get request are processed

--- steam.py
+++ steam.py
@@ -29,55 +29,77 @@ def make_steam_request(steam_url, endpoint, version, payload):
 
 
 def get_persona_name(steam_id):
-   # get custom player summary using key and provided user ID
-   payload = {
+
+    # get custom player summary using key and provided user ID
+    payload = {
        'key': STEAM_API_KEY,
        'steamids': steam_id,
-   }
-   json_response = make_steam_request(
+    }
+    json_response = make_steam_request(
        steam_url = STEAM_USER_URL,
        endpoint = "GetPlayerSummaries",
        version = VERSION,
        payload = payload,
-   )
-   # drill down into player summary JSON and get persona name
-   persona_name = json_response["response"]["players"][0]["personaname"]
-   # return the persona name
-   return persona_name
+    )
+    # drill down into player summary JSON and get persona name
+    persona_name = json_response["response"]["players"][0]["personaname"]
+    # return the persona name
+    return persona_name
 
 def get_avatar(steam_id):
-   # get custom player summary using key and provided user ID
-   payload = {
+    # get custom player summary using key and provided user ID
+    payload = {
        'key': STEAM_API_KEY,
        'steamids': steam_id,
-   }
-   json_response = make_steam_request(
+    }
+    json_response = make_steam_request(
        steam_url = STEAM_USER_URL,
        endpoint = "GetPlayerSummaries",
        version = VERSION,
        payload = payload,
-   )
-   # drill down into player summary JSON and get avatar
-   avatar = json_response["response"]["players"][0]["avatarfull"]
-   # return the avatar
-   return avatar
+    )
+    # drill down into player summary JSON and get avatar
+    avatar = json_response["response"]["players"][0]["avatarfull"]
+    # return the avatar
+    return avatar
 
 
 def get_friends_list(steam_id):
-   # get custom friends list using key and provided user ID
-
-   friends_json =  requests.get("http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key={0}&steamid={1}&relationship=friend".format( STEAM_API_KEY, steam_id)).json()
-   # drill down into JSON and get friends list
-   friends_list = friends_json["friendslist"]["friends"]
-   # return the friends list
-   return friends_list
+    # get custom friends list using key and provided user ID
+    payload = {
+       'key': STEAM_API_KEY,
+       'steamid': steam_id,
+       'relationship': 'friend',
+    }
+    json_response = make_steam_request(
+       steam_url = STEAM_USER_URL,
+       endpoint = "GetFriendList",
+       version = 'v0001',
+       payload = payload,
+    )
+    # drill down into JSON and get friends list
+    friends_list = json_response["friendslist"]["friends"]
+    # return the friends list
+    return friends_list
 
 
 def get_game_list(steam_id):
-   # get custom games list using key and provided user ID
-   games_json =  requests.get("http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={0}&steamid={1}&include_appinfo=1".format( STEAM_API_KEY, steam_id)).json()
-   # drill down into JSON and get games list and count
-   game_list = games_json["response"]["games"]
-   game_count = games_json["response"]["game_count"]
-   # return the games list
-   return {'count': game_count ,'games': game_list}
+    # get custom games list using key and provided user ID
+    payload = {
+        'key': STEAM_API_KEY,
+        'steamid': steam_id,
+        'include_appinfo': 1,
+        'include_played_free_games':1,
+
+    }
+    json_response = make_steam_request(
+        steam_url = STEAM_PLAYER_URL,
+        endpoint = "GetOwnedGames",
+        version = 'v0001',
+        payload = payload,
+    )
+    # drill down into JSON and get games list and count
+    game_list = json_response["response"]["games"]
+    game_count = json_response["response"]["game_count"]
+    # return the games list
+    return {'count': game_count ,'games': game_list}
