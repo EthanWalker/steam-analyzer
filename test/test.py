ef402682ae3d2e99ef9cf55a0dcef593ed00412e
Ethan Walker <ethan@foundationtech.us>
2017-08-20 20:38:54 -0500

    switched out test case where profile had gone private

--- test/test.py
+++ test/test.py
@@ -3,12 +3,12 @@ from steam import (
     get_friends,
     get_games,
     get_persona,
-    get_top_game_count,
+    get_top_game_counts,
 )
 import pytest
 
 TEST_IDS = [76561197960435530,
-            76561197972495328,
+            76561197997951123,
             76561197960434622,]
 
 
@@ -16,7 +16,7 @@ def test_packaging():
     assert True
 
 def test_get_persona():
-    tests = zip(TEST_IDS, ["Robin", "ChrisK", "al",])
+    tests = zip(TEST_IDS, ["Robin", "randable", "al",])
     for id, persona in tests:
         assert get_persona(id) == persona
 
@@ -35,7 +35,7 @@ def test_avatar_not_present():
         result = get_avatar(12345646546572145)
 
 def test_get_friends():
-    tests = zip(TEST_IDS, [287, 211, 160])
+    tests = zip(TEST_IDS, [287, 6, 160])
     for id, friends in tests:
         assert len(get_friends(id)) >= friends
 
@@ -44,7 +44,7 @@ def test_friends_not_present():
         result = get_friends(12345646546572145)
 
 def test_get_games():
-    tests = zip(TEST_IDS, [493, 878, 595])
+    tests = zip(TEST_IDS, [493, 228, 595])
     for id, games in tests:
         assert get_games(id)["count"] == len(get_games(id)["games"])
         assert get_games(id)["count"] >= games
